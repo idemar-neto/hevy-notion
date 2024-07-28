747f9773029ec10eeef55df63c1e376a24c3a97e
Idemar Neto <idemarneto13@gmail.com>
2024-07-27 21:19:19 -0300

    Ajustes vercel

--- api/update_notion.py
+++ api/update_notion.py
@@ -1,37 +1,9 @@
 import os
 import requests
 from dotenv import load_dotenv
-from http.server import BaseHTTPRequestHandler
-load_dotenv()
- 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        response = handler({})
-        self.send_response(response['statusCode'])
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(response['body'].encode("utf-8"))
-        return
-    
-def handler(request):
-    hevy_data = fetch_hevy_data()
-    if hevy_data:
-        if check_last_id(hevy_data):
-            return {
-                "statusCode": 500,
-                "body": "Notion not updated with Hevy data due to not having new workouts"
-            }
-        update_notion(hevy_data)
-        return {
-            "statusCode": 200,
-            "body": "Notion updated successfully with Hevy data"
-        }
-    else:
-        return {
-            "statusCode": 500,
-            "body": "Error fetching Hevy data"
-        }
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+load_dotenv()
 
 NOTION_API_URL = "https://api.notion.com/v1/pages/"
 NOTION_API_URL_BLOCK = "https://api.notion.com/v1/blocks/"
@@ -42,7 +14,7 @@ HEVY_API_KEY = os.getenv("HEVY_API_KEY")
 
 notion_headers = {
     "Authorization": f"Bearer {NOTION_TOKEN}",
-    "Connection" : "keep-alive",
+    "Connection": "keep-alive",
     "Content-Type": "application/json",
     "Notion-Version": "2022-02-22"
 }
@@ -60,7 +32,6 @@ def read_last_workout_id(file_path='api/last_workout_id.txt'):
     except FileNotFoundError:
         return None
 
-
 def fetch_hevy_data():
     try:
         response = requests.get(HEVY_API_URL, headers=hevy_headers)
@@ -72,16 +43,16 @@ def fetch_hevy_data():
     except Exception as e:
         print(f"Exception fetching Hevy data: {e}")
         return None
-    
+
 def check_last_id(data):
     for workout in data['workouts']:
         if workout['id'] == read_last_workout_id():
             return True
+    return False
 
 def save_last_workout_id(workout_id, file_path='api/last_workout_id.txt'):
     with open(file_path, 'w') as file:
         file.write(workout_id)
-
 
 def update_notion(data):
     for workout in data['workouts']:
@@ -101,59 +72,86 @@ def update_notion(data):
         if response.status_code != 200:
             print(f"Error updating Notion: {response.text}")
         save_last_workout_id(workout["id"])
-    
+
 def payload_treino(treino):
     return {
         "children": [
-		{
-			"object": "block",
-			"type": "heading_2",
-			"heading_2": {
-				"rich_text": [{ "type": "text", "text": { "content": treino['title'] } }]
-			}
-        },
-        {
-            "object": "block",
-            "type": "paragraph",
-            "paragraph": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": format_workout_description(treino)
+            {
+                "object": "block",
+                "type": "heading_2",
+                "heading_2": {
+                    "rich_text": [{ "type": "text", "text": { "content": treino['title'] } }]
+                }
+            },
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": format_workout_description(treino)
+                            }
                         }
-                    }
-                ]
+                    ]
+                }
             }
-        }
-		]
+        ]
     }
 
 def format_workout_description(workout_data):
     description = []
-        
+
     for exercise in workout_data['exercises']:
         description.append(exercise['title'])
         for i, set_info in enumerate(exercise['sets']):
             set_line = f"Série {i + 1}: "
-            
             if 'weight' in set_info:
                 set_line += f"{set_info['weight_kg']} kg x "
-            
             set_line += f"{set_info['reps']} "
-            
             if 'rpe' in set_info:
                 set_line += f"@ {set_info['rpe']} rpe"
-            
             if 'set_type' in set_info:
                 if set_info['set_type'] != "normal":
                     set_line += f" [{set_info['set_type']}]"
-            
             description.append(set_line)
         description.append("")
-    
+
     return "\n".join(description)
 
-if __name__ == "__main__":
-    print(handler({}))
+def handler(request):
+    hevy_data = fetch_hevy_data()
+    if hevy_data:
+        if check_last_id(hevy_data):
+            return {
+                "statusCode": 500,
+                "body": "Notion not updated with Hevy data due to not having new workouts"
+            }
+        update_notion(hevy_data)
+        return {
+            "statusCode": 200,
+            "body": "Notion updated successfully with Hevy data"
+        }
+    else:
+        return {
+            "statusCode": 500,
+            "body": "Error fetching Hevy data"
+        }
 
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        response = handler({})
+        self.send_response(response['statusCode'])
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(response['body'].encode("utf-8"))
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
